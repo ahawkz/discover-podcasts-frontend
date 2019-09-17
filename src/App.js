ceb4c09980a90b369b50c08547420f6bd6c6edbd
Alex <ahawknj15@gmail.com>
2019-09-17 14:39:53 -0400

    search component created and search works, but API not rendering a url that has data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Test from './components/Test.js'
+import Search from './components/Search.js'
 
 function App() {
   return (
     <div className="App">
       < Test/>
+      < Search />
     </div>
   );
 }
