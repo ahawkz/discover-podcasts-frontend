11b6d6b7662d97d1b2283d2ed7bf3995cd664d9a
Alex <ahawknj15@gmail.com>
2019-09-17 10:39:14 -0400

    adding to github

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Test from './components/Test.js'
+
+function App() {
+  return (
+    <div className="App">
+      < Test/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Test extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {message: ''}
+  }
+  componentDidMount(){
+    fetch('/test')
+    .then(data => {
+      return data.json()
+    })
+    .then(json => {
+      console.log(json)
+      this.setState({
+        message: json
+      })
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+  render(){
+    return(
+      <div>
+        <h1>hello world</h1>
+        <h2> {this.state.message.message} </h2>
+      </div>
+    )
+  }
+}
+
+export default Test;
