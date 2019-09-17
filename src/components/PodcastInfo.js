b51fab130144c823c4ade68d720b33d87e5c5883
Alex <ahawknj15@gmail.com>
2019-09-17 19:18:10 -0400

    switched api to itunes api and can succesfully fetch and display data

--- src/components/PodcastInfo.js
+++ src/components/PodcastInfo.js
@@ -1,9 +1,9 @@
-class PodcastInfo extends Component {
-  render() {
-    return(
-      <div>
-        
-      </div>
-    )
-  }
-}
+// class PodcastInfo extends Component {
+//   render() {
+//     return(
+//       <div>
+//         <h1>Title: {this.props.podcast}</h1>
+//       </div>
+//     )
+//   }
+// }
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,11 +4,11 @@ class Search extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      baseURL: 'https://listen-api.listennotes.com/api/v2/search?',
-      query:'q=',
-      podcastTitle: '',
-      remainderURL: '&sort_by_date=0&type=podcast&offset=0&len_min=10&len_max=60&genre_ids=68%2C82&published_before=1390190241000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=1',
-      searchURL: ''
+      baseURL: 'https://itunes.apple.com/search?',
+      query:'term=',
+      searchItem: '',
+      entity: '&entity=podcast',
+      searchURL: '',
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -19,24 +19,18 @@ class Search extends React.Component {
   handleSubmit (event) {
     event.preventDefault()
     this.setState({
-      searchURL: this.state.baseURL + this.state.query + this.state.podcastTitle + this.state.remainderURL
+      searchURL: this.state.baseURL + this.state.query + this.state.searchItem + this.state.entity
     }, () => {
-      fetch(this.state.searchURL, {
-        method: 'GET',
-        withCredentials: true,
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-          'X-ListenAPI-Key': '92eeeea0eefc42ec8913d237dedca5da'
-        }
-      })
+      fetch(this.state.searchURL)
         .then(response => response.json())
-        .then(json => this.setState(
+        .then(json => {
+          console.log(json)
+          this.setState(
           {
             podcast: json,
-            podcastTitle: ''
+            searchItem: ''
           }
-        ), err => console.log(err))
+        )}, err => console.log(err))
     })
   }
   render(){
@@ -44,11 +38,11 @@ class Search extends React.Component {
       <React.Fragment>
         <h1>discoverPods</h1>
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor='podcastTitle'>Search</label>
+          <label htmlFor='searchItem'>Search</label>
           <input
-            id='podcastTitle'
+            id='searchItem'
             type='text'
-            value={this.state.podcastTitle}
+            value={this.state.searchItem}
             onChange={this.handleChange}
           />
           <input
@@ -56,7 +50,7 @@ class Search extends React.Component {
             value="Find Podcast Info"
           />
         </form>
-        {this.state.podcast.results[0]}
+        { (this.state.podcast) ? this.state.podcast.results[0].artistName : null }
       </React.Fragment>
     )
   }
