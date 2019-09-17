db0b345264b6ab35196481ca47b392a5f6159541
Alex <ahawknj15@gmail.com>
2019-09-17 10:51:49 -0400

    added proxy file

---
+++ src/setupProxy.js
@@ -0,0 +1,6 @@
+const proxy = require('http-proxy-middleware');
+
+   module.exports = function(app) {
+       app.use(proxy('/api/**', { target: "https://discover-podcasts.herokuapp.com/" }));
+       // app.use(proxy('/otherApi/**', { target: 'http://localhost:5000' }));
+   };
