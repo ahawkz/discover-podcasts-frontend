547952ef058d2892b0c0d41b56262370adece1f3
Alex <ahawknj15@gmail.com>
2019-09-18 15:45:23 -0400

    styled header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,14 @@ import React from 'react';
 class Header extends React.Component {
   render(){
     return(
-      <h1>hello</h1>
+      <header className="header">
+        <a href=""><h4>myNews</h4></a>
+        <div className="social-icons">
+          <a href=""><h6>icon</h6></a>
+          <a href=""><h6>icon</h6></a>
+          <a href=""><h6>icon</h6></a>
+        </div>
+      </header>
     )
   }
 }
