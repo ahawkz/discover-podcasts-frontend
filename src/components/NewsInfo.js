718aa945a7dda82ff674bbf0ab3a44bfd8973062
Alex <ahawknj15@gmail.com>
2019-09-18 14:22:46 -0400

    added links to headlines and they open in new tab

--- src/components/NewsInfo.js
+++ src/components/NewsInfo.js
@@ -6,7 +6,13 @@ class NewsInfo extends React.Component {
       <div>
         <h1>hello this is your news</h1>
         {this.props.news.map((item, key)  =>
+
+          <a key={key} href={item.web_url} target="_blank" rel="noopener noreferrer">
+
           <h4 key={key}>{item.snippet}</h4>
+
+          </a>
+
         )}
       </div>
     )
